3b689cb7d4df87fbd17bcfc634b55e3a6d65c44f
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-10-28 14:11:59 +0100

    new joystick code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
 
+#define JOY_BUT 9
+#define JOY_Y A1
+#define JOY_X A0
+
 void setup() {
-  // put your setup code here, to run once:
+
+    Serial.begin(9600);
+
+    pinMode(JOY_BUT, INPUT);
+    pinMode(JOY_Y, INPUT);
+    pinMode(JOY_X, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int joyY = analogRead(JOY_Y);
+    int joyX = analogRead(JOY_X);
+
+    Serial.println((String)"Joystick X: " + (joyX - 506) + ", Y: " + (joyY - 491));
+
+    if (digitalRead(JOY_BUT) == HIGH) {
+        Serial.println("button");
+    }
 }
